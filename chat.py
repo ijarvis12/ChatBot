2f03f11d501fbda5947be50e71a29589163e43f0
Ian Jarvis <ijarvis12@gmail.com>
2019-08-12 08:33:56 -0500

    adding text-to-speech capabilities

--- chat.py
+++ chat.py
@@ -2,13 +2,14 @@
 
 from string import whitespace,punctuation
 from random import seed,randint
+import pyttsx3
 
 # databases of responses, prepopulated
 database = [ ["hello",[0,1]],
 			 ["hi",[0,1]] ]
 
-def analyze(query,previ):
-#	nicer input
+def cleanup(query):
+	#	nicer input
 	query = query.lower()
 	query = query.strip()
 	query = " ".join(query.split())
@@ -25,11 +26,11 @@ def analyze(query,previ):
 		if s not in punctuation:
 			query3 += s
 
-#	cleanup
-	query = query3
-	del query2
-	del query3
-	
+	return query3
+
+
+
+def analyze(query,previ):	
 #	loop through database for response
 	for i,data in enumerate(database):
 #		if in the database, print response
@@ -45,7 +46,12 @@ def analyze(query,previ):
 			outnum = data[1][randint(0,len(data[1]))-1]
 #			get reponse
 			out = database[outnum][0]
+#			print response to terminal
 			print(out)
+#			speak response to speakers
+			engine.say(out)
+			engine.runAndWait()
+#			break out of for loop
 			break
 			
 #	else append query to database with previous response
@@ -62,13 +68,35 @@ def analyze(query,previ):
 
 
 
-# main program
+# main program start
 
+#starting necesseties
 seed()
-print("print 'hello' or 'hi' to start")
-query = input("> ")
-previ = analyze(query,0)
+engine = pyttsx3.init()
+# getting details of current speaking rate
+rate = engine.getProperty('rate')
+#set speaking rate
+engine.setProperty('rate', 160)
+#get details of current voice
+voices = engine.getProperty('voices')
+#set voice
+engine.setProperty('voice', voices[3].id)
+
+#greeting, and off we go!
+print("hello")
+engine.say("hello")
+engine.runAndWait()
+previ = 0
+
+#main loop
 while True:
+#	get user input
 	query = input("> ")
+#	cleanup the query
+	query = cleanup(query)
+	#check for exit command
+	if query == 'exit':
+		break
+#	else analyze query and set index for next loop iteration
 	previ = analyze(query,previ)
 	
