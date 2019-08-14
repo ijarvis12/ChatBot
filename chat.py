8f45490fc102fc0496adec2c53bad1f4824a6987
Ian Jarvis <ijarvis12@gmail.com>
2019-08-14 11:54:08 -0500

    making algorithm more sane

--- chat.py
+++ chat.py
@@ -5,11 +5,12 @@ from random import seed,randint
 import pyttsx3
 
 # databases of responses, prepopulated (add more as we go)
-database = [ ["hello",[0,1]],
-			 ["hi",[0,1]] ]
+#			  repsonse,previ,nexti
+database = [ ["hello",[0,1],[0,1]],
+			 ["hi",[0,1],[0,1]] ]
 
 def cleanup(query):
-	#	nicer input
+#	nicer input
 	query = query.lower()
 	query = query.strip()
 	query = " ".join(query.split())
@@ -31,38 +32,54 @@ def cleanup(query):
 
 
 def analyze(query,previ):	
+
 #	loop through database for response
 	for i,data in enumerate(database):
-#		if in the database, print response
+
+#		find if in the database
 		if query == data[0]:
+
 #			set previous query iteration
 			if len(data[1]) < 48:
 				database[i][1].append(previ)
 			else:
 				database[i][1][i%48] = previ
 
+#			set next query iteration for the previous
+			if len(data[2]) < 48:
+				database[previ][2].append(i)
+			else:
+				database[previ][2][previ%48] = i
+
 #			set var for next query iteration
 			previ = i
 
-#			random response from given choices in database	        
-			outnum = data[1][randint(0,len(data[1]))-1]
+#			if next responses exist...
+			if len(data[2]) > 0:
+#				...get random response num from given database choices
+				outnum = data[2][randint(0,len(data[2]))-1]
 
-#			get reponse
-			out = database[outnum][0]
+#				get reponse
+				out = database[outnum][0]
 
-#			print response to terminal
-			print(out)
+#				print response to terminal
+				print(out)
 
-#			speak response to speakers
-			engine.say(out)
-			engine.runAndWait()
+#				speak response to speakers
+				engine.say(out)
+				engine.runAndWait()
 
 #			break out of for loop
 			break
 			
-#	else append query to database with previous response
 	else:
-		database.append([query,[previ]])
+
+#		else append query to database with previous response
+		database.append([query,[previ],[]])
+
+#		add next query response to previous
+		database[previ][2].append(len(database)-1)
+
 #		set var for next query iteration
 		previ = len(database)-1
 
