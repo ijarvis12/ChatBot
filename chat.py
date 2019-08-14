411f853b712c58dceac8172b29ecc908c2866603
Ian Jarvis <ijarvis12@gmail.com>
2019-08-14 01:26:15 -0500

    made code more readable

--- chat.py
+++ chat.py
@@ -4,7 +4,7 @@ from string import whitespace,punctuation
 from random import seed,randint
 import pyttsx3
 
-# databases of responses, prepopulated
+# databases of responses, prepopulated (add more as we go)
 database = [ ["hello",[0,1]],
 			 ["hi",[0,1]] ]
 
@@ -40,43 +40,49 @@ def analyze(query,previ):
 				database[i][1].append(previ)
 			else:
 				database[i][1][i%48] = previ
+
 #			set var for next query iteration
 			previ = i
+
 #			random response from given choices in database	        
 			outnum = data[1][randint(0,len(data[1]))-1]
+
 #			get reponse
 			out = database[outnum][0]
+
 #			print response to terminal
 			print(out)
+
 #			speak response to speakers
 			engine.say(out)
 			engine.runAndWait()
+
 #			break out of for loop
 			break
 			
 #	else append query to database with previous response
 	else:
 		database.append([query,[previ]])
-		
 #		set var for next query iteration
 		previ = len(database)-1
-
 
 	return previ
 
 
 
 
+#					 #
+# main program start #
+#					 #
 
-# main program start
 
 #starting necesseties
 seed()
 engine = pyttsx3.init()
-# getting details of current speaking rate
-rate = engine.getProperty('rate')
+
 #set speaking rate
 engine.setProperty('rate', 160)
+
 #get details of current voice
 voices = engine.getProperty('voices')
 #set voice
@@ -94,9 +100,9 @@ while True:
 	query = input("> ")
 #	cleanup the query
 	query = cleanup(query)
-	#check for exit command
+#	check for exit command
 	if query == 'exit':
 		break
 #	else analyze query and set index for next loop iteration
 	previ = analyze(query,previ)
-	
+
